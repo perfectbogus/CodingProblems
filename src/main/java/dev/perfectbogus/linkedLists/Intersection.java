2b67606eb431bf103520640593bd2829ee221da2
eduardo escobar <war_army_10@hotmail.com>
2020-10-28 12:15:25 -0600

    Added another approach to solve Intersection problem

--- src/main/java/dev/perfectbogus/linkedLists/Intersection.java
+++ src/main/java/dev/perfectbogus/linkedLists/Intersection.java
@@ -19,8 +19,63 @@ public class Intersection {
 
         BookLinkedListNode result = intersection(aList, bList);
         System.out.println(result.toString());
+        BookLinkedListNode resultInter = findIntersection(aList, bList);
+        System.out.println(resultInter.toString());
     }
 
+    public static BookLinkedListNode findIntersection(BookLinkedListNode aList, BookLinkedListNode bList) {
+        if (aList == null || bList == null) return null;
+
+        NodeStats resultA = getNodeStats(aList);
+        NodeStats resultB = getNodeStats(bList);
+
+        if (resultA.tail != resultB.tail) {
+            return null;
+        }
+
+        BookLinkedListNode shorter = resultA.size < resultB.size ? aList : bList;
+        BookLinkedListNode longer = resultA.size < resultB.size ? bList : aList;
+
+        longer = getKthNode(longer, Math.abs(resultA.size - resultB.size));
+        while (shorter != longer) {
+            shorter = shorter.next;
+            longer = longer.next;
+
+        }
+
+        return longer;
+    }
+
+    public static BookLinkedListNode getKthNode(BookLinkedListNode node, int kth) {
+        BookLinkedListNode current = node;
+        while (kth > 0 && current != null) {
+            current = current.next;
+            kth--;
+        }
+        return current;
+    }
+
+    public static NodeStats getNodeStats(BookLinkedListNode node) {
+        if (node == null) return null;
+
+        int size = 1;
+        BookLinkedListNode current = node;
+        while (current.next != null) {
+            size++;
+            current = current.next;
+        }
+        return new NodeStats(current, size);
+    }
+
+    static class NodeStats {
+        public BookLinkedListNode tail;
+        public int size;
+
+        public NodeStats(BookLinkedListNode tail, int size) {
+            this.tail = tail;
+            this.size = size;
+        }
+    }
 
     /**
      * My Solution
