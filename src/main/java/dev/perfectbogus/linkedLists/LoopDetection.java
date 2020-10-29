3709ebde5ee8ed181bf10ac20b8dfa0ddf12461d
eduardo escobar <war_army_10@hotmail.com>
2020-10-29 13:25:35 -0600

    Added Loop Detection Problem

---
+++ src/main/java/dev/perfectbogus/linkedLists/LoopDetection.java
@@ -0,0 +1,14 @@
+package dev.perfectbogus.linkedLists;
+
+import dev.perfectbogus.linkedLists.Partition.BookLinkedListNode;
+
+public class LoopDetection {
+
+    public static void main(String[] args){
+
+    }
+
+    public static boolean loopDetection(BookLinkedListNode node){
+        return true;
+    }
+}
