d2000751b3dcb533fab4cb7098b5ea7ba403e4ce
eduardo escobar <war_army_10@hotmail.com>
2020-10-30 15:23:36 -0600

    Solution for loop detection

--- src/main/java/dev/perfectbogus/linkedLists/LoopDetection.java
+++ src/main/java/dev/perfectbogus/linkedLists/LoopDetection.java
@@ -8,7 +8,28 @@ public class LoopDetection {
 
     }
 
-    public static boolean loopDetection(BookLinkedListNode node){
-        return true;
+    public static BookLinkedListNode loopDetection(BookLinkedListNode head){
+        BookLinkedListNode slow = head;
+        BookLinkedListNode fast = head;
+
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if( slow == fast){
+                break;
+            }
+        }
+
+        if (fast == null || fast.next == null){
+            return null;
+        }
+
+        slow = head;
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return fast;
     }
 }
