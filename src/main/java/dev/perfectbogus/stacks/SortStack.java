d7c45f0d6778cb3268f06905625de100b51f9588
eduardo escobar <war_army_10@hotmail.com>
2021-01-13 16:13:53 -0600

    Solving: Sort Stack using only Stacks

---
+++ src/main/java/dev/perfectbogus/stacks/SortStack.java
@@ -0,0 +1,57 @@
+package dev.perfectbogus.stacks;
+
+import java.util.Stack;
+
+/**
+ * Sort a Stack:
+ * <p>
+ * Write a program to sort a stack that the smallest items are on the top. You can use a temporary stack, but
+ * you may not copy the elements into any another data structure (such as array). The stack supports the following
+ * operations: push, pop, peek, and isEmpty.
+ * <p>
+ * Approaches:
+ * pop the element on the input stack after that use the second stack to store that value
+ * after that pop the next element and compare with the first value of the other stack
+ * if
+ */
+
+public class SortStack {
+
+    public static void sort(Stack<Integer> inputStack) {
+        Stack<Integer> swapStack = new Stack<>();
+        while (inputStack.size() > 0) {
+            Integer data = inputStack.pop();
+            if (swapStack.isEmpty()) {
+                swapStack.push(data);
+            } else {
+                Integer peekData = swapStack.peek();
+                if (data >= peekData) {
+                    swapStack.push(data);
+                } else {
+                    int indexPopped = 0;
+                    while (peekData > data){
+                        peekData = swapStack.pop();
+                        inputStack.push(peekData);
+                        indexPopped++;
+                        if (swapStack.size() > 0){
+                            peekData = swapStack.peek();
+                        } else {
+                            break;
+                        }
+                    }
+                    swapStack.push(data);
+                    while (indexPopped > 0) {
+                        int tempData = inputStack.pop();
+                        swapStack.push(tempData);
+                        indexPopped--;
+                    }
+                }
+            }
+        }
+        while(!swapStack.isEmpty()){
+            int tempData = swapStack.pop();
+            inputStack.push(tempData);
+        }
+    }
+
+}
---
+++ src/test/java/dev/perfectbogus/stacks/SortStackTests.java
@@ -0,0 +1,30 @@
+package dev.perfectbogus.stacks;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import java.util.Stack;
+
+public class SortStackTests {
+
+    @Test
+    public void sortTest(){
+        Stack<Integer> myStack = new Stack<>();
+
+        myStack.push(5);
+        myStack.push(30);
+        myStack.push(10);
+        myStack.push(18);
+        myStack.push(3);
+
+        SortStack.sort(myStack);
+        int peekData = myStack.pop()    ;
+        while (!myStack.isEmpty()){
+            int nextData = myStack.pop();
+            assertEquals(true, peekData < nextData);
+            peekData = nextData;
+        }
+    }
+}
