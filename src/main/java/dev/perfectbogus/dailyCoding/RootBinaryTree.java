595e82d159cce838ec55f290e4182fb715a83d52
eduardo escobar <war_army_10@hotmail.com>
2021-06-17 19:06:39 -0500

    Solving Serialized and Deserialized problem

--- src/main/java/dev/perfectbogus/dailyCoding/RootBinaryTree.java
+++ src/main/java/dev/perfectbogus/dailyCoding/RootBinaryTree.java
@@ -1,5 +1,8 @@
 package dev.perfectbogus.dailyCoding;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RootBinaryTree {
 
     /**
@@ -10,34 +13,52 @@ public class RootBinaryTree {
      * For example, given the following Node class
      */
     public static void main(String[] args){
-        Node<Integer> root = new Node<>(1, new Node<>(2, null, null), null);
-        System.out.println(serialize(root));
+        Node<Integer> root = new Node<>(1, new Node<>(2, new Node<>(3 , null, null), null), new Node<>(3, null, null));
+        String serRoot1 = serialize(root);
+        System.out.println(serRoot1);
+
+        Node<Integer> root2 = new Node<>(1, null, null);
+        String serRoot = serialize(root2);
+        System.out.println(serRoot);
+
+        Node<Integer> des = deserialize(serRoot);
+        Node<Integer> des1 = deserialize(serRoot1);
     }
 
     public static String serialize(Node<Integer> root){
         if(root == null){
-            return "( null ) ";
+            return "( null )";
         } else {
             return "( " + root.val + " " + serialize(root.left) + " " + serialize(root.right) + " )";
         }
-//        str.append("( ");
-//        str.append(temp.val);
-//        str.append(" ");
-//        str.append("( ");
-//        str.append(root.left);
-//        str.append(" ");
-//        str.append(") ");
-//        str.append("( ");
-//        str.append(root.right);
-//        str.append(" ");
-//        str.append(") ");
-//        str.append(") ");
     }
 
-    public static Node<Integer> deserialize(String rootSerialized){
+    public static Node<Integer> deserialize(String str){
+        System.out.println(str);
+        if(str.equals("( null )")){
+            return null;
+        } else {
+            String pattern = "\\( ([0-9]*) \\( (.*)+ \\) \\( (.*)+ \\) \\)";
+            Pattern r = Pattern.compile(pattern);
+            Matcher matcher = r.matcher(str);
+            String[] matches = new String[4];
 
-        return new Node<>(1);
+            while (matcher.find()) {
+                matches[0] = matcher.group(0);
+                for (int i = 1; i <= matcher.groupCount(); i++) {
+                    System.out.println("Group " + i + ": " + matcher.group(i));
+                    matches[i] = matcher.group(i);
+                }
+            }
+
+            return new Node<>(Integer.parseInt(matches[1]), deserialize("( " + matches[2] + " )"), deserialize("( " + matches[3]  + " )"));
+        }
     }
+
+    public static class NotValidStringSerializedException extends Exception {
+
+    }
+
     public static class Node<T>{
         public T val;
         public Node<T> left;
