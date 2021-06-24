5e40adb970ad98f5ec969e0a4b6e441b26d6f03d
eduardo escobar <war_army_10@hotmail.com>
2021-06-23 19:43:55 -0500

    Adding solution of the author for ProducNumbers

---
+++ src/main/java/dev/perfectbogus/dailyCoding/NonAdjacentSumHard.java
@@ -0,0 +1,72 @@
+package dev.perfectbogus.dailyCoding;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class NonAdjacentSumHard {
+
+    /**
+     * Given a list of integers, write a function that returns the largest sum of non-adjacent numbers.
+     * Numbers can be 0 or negative.
+     *
+     * For example,
+     *
+     * [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5.
+     *
+     * [5, 1, 1, 5] should return 10, since we pick 5 and 5.
+     *
+     * Follow-up: Can you do this in O(N) time and constant space?
+     * @param args
+     */
+
+    public static void main(String[] args){
+        int[] list = {2, 4, 6, 2, 5};
+        /*
+        2,6,5
+        2,2
+        4,2
+        2,6,5
+
+         */
+        int[] list2 = {5, 1, 5, 1};
+        int otherInt = anotherSolution(list);
+        System.out.println(otherInt);
+
+        int sol = simpleSolution(list);
+        System.out.println(sol);
+        int sol2 = simpleSolution(list2);
+        System.out.println(sol2);
+
+        Collections.reverse(Arrays.asList(list2));
+        System.out.println();
+    }
+
+    public static int simpleSolution(int[] list){
+        int biggest = Integer.MIN_VALUE;
+        for(int pivot = 0 ; pivot < list.length / 2; pivot++){
+            for(int space = 2; space < list.length ; space++){
+                int mult = 0;
+                int sum = 0;
+                while(mult * space < list.length){
+                    sum += list[mult * space];
+                    mult++;
+                }
+                if(sum > biggest){
+                    biggest = sum;
+                }
+            }
+        }
+        return biggest;
+    }
+
+    public static int anotherSolution(int[] arr){
+        int s2 = Math.max(Integer.MIN_VALUE, arr[0]);
+        int s1 = Math.max(s2, arr[1]);
+        for(int k = 2; k < arr.length; k++){
+             int s0 = Math.max(s1, s2 + arr[k]);
+            s2 = s1;
+            s1 = s0;
+        }
+        return s1;
+    }
+}
--- src/main/java/dev/perfectbogus/dailyCoding/ProductNumbers.java
+++ src/main/java/dev/perfectbogus/dailyCoding/ProductNumbers.java
@@ -1,5 +1,10 @@
 package dev.perfectbogus.dailyCoding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Given an array of integers, return a new array such that each element at index i of the new array is the product
  * of all the numbers in the original array except the one at i.
@@ -10,6 +15,12 @@ package dev.perfectbogus.dailyCoding;
  * Follow-up: what if you can't use division?
  */
 public class ProductNumbers {
+
+    public static void main(String[] args){
+        int[] nums = {1,2,3,4,5};
+        List<Integer> result = authorSolution(nums);
+        System.out.println(result);
+    }
 
     public static int[] nonDivisionSolution(int[] data){
         int[] output = new int[data.length];
@@ -41,4 +52,52 @@ public class ProductNumbers {
         }
         return out;
     }
+
+    public static int[] reverse(int[] nums){
+        int[] aux = new int[nums.length];
+        int j = 0;
+        for(int i = nums.length - 1; i >= 0; i--  ){
+            aux[j] = nums[i];
+            j++;
+        }
+        return aux;
+    }
+
+    public static List<Integer> authorSolution(int[] nums){
+        List<Integer> prefixProducts = new ArrayList<>();
+        for(int num : nums){
+            if( prefixProducts.size() > 0 ){
+                int mult = prefixProducts.get(prefixProducts.size() - 1 ) * num;
+                prefixProducts.add(mult);
+            } else {
+                prefixProducts.add(num);
+            }
+        }
+        List<Integer> suffixProducts = new ArrayList<>();
+        int[] reversedNums = reverse(nums) ;
+
+        for(int num: reversedNums){
+            if( suffixProducts.size() > 0){
+                int mult = suffixProducts.get(prefixProducts.size() - 1) * num;
+                prefixProducts.add(mult);
+            } else {
+                suffixProducts.add(num);
+            }
+        }
+
+        Collections.reverse(suffixProducts);
+
+        List<Integer> result = new ArrayList<>();
+        for(int i = 0; i < nums.length ; i++){
+            if(i == 0){
+                result.add(suffixProducts.get(i + 1));
+            } else if( i == nums.length - 1 ){
+                result.add(prefixProducts.get(i - 1));
+            } else {
+                result.add(prefixProducts.get(i -1) * suffixProducts.get(i + 1));
+            }
+        }
+        return result;
+    }
 }
+
