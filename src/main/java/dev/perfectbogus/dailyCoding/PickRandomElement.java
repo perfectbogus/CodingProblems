f08344e881c7e9054b7aa3f015e0ea3796fb8c64
eduardo escobar <war_army_10@hotmail.com>
2021-06-28 16:27:53 -0500

    Solving Randomless problems

---
+++ src/main/java/dev/perfectbogus/dailyCoding/PickRandomElement.java
@@ -0,0 +1,20 @@
+package dev.perfectbogus.dailyCoding;
+
+public class PickRandomElement {
+
+
+    /**
+     *
+     * Given a stream of elements too large to store in memory, pick a random element
+     * from the stream with uniform probability.
+     */
+    public static void main(String[] args){
+
+    }
+
+    public static int pick(int[] stream){
+        int randomElement;
+        int split = 3;
+        return 0;
+    }
+}
